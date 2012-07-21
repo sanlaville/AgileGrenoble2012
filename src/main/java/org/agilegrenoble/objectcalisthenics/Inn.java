933f2fd9870de06770f6711063fcdac1a955765a
Remy Sanlaville <remy.sanlaville@gmail.com>
2012-07-21 23:14:29 +0200

    Apply Rule 1

--- src/main/java/org/agilegrenoble/objectcalisthenics/Inn.java
+++ src/main/java/org/agilegrenoble/objectcalisthenics/Inn.java
@@ -11,68 +11,76 @@ public class Inn {
 
 	public void updateQuality() {
 		for (int i = 0; i < items.size(); i++) {
-			if (!items.get(i).getName().equals("Aged Brie")
-					&& !items
-							.get(i)
-							.getName()
-							.equals("Backstage passes to a TAFKAL80ETC concert")) {
-				if (items.get(i).getQuality() > 0) {
-					if (!items.get(i).getName()
-							.equals("Sulfuras, Hand of Ragnaros")) {
-						items.get(i).setQuality(items.get(i).getQuality() - 1);
+			updateQuality(i);
+		}
+	}
+
+	/** Update quality for item i
+	 * 
+	 * @param i index of the item. 0 <= i < items.size();
+	 */
+	private void updateQuality(int i) {
+		if (!items.get(i).getName().equals("Aged Brie")
+				&& !items
+						.get(i)
+						.getName()
+						.equals("Backstage passes to a TAFKAL80ETC concert")) {
+			if (items.get(i).getQuality() > 0) {
+				if (!items.get(i).getName()
+						.equals("Sulfuras, Hand of Ragnaros")) {
+					items.get(i).setQuality(items.get(i).getQuality() - 1);
+				}
+			}
+		} else {
+			if (items.get(i).getQuality() < 50) {
+				items.get(i).setQuality(items.get(i).getQuality() + 1);
+
+				if (items
+						.get(i)
+						.getName()
+						.equals("Backstage passes to a TAFKAL80ETC concert")) {
+					if (items.get(i).getSellIn() < 11) {
+						if (items.get(i).getQuality() < 50) {
+							items.get(i).setQuality(
+									items.get(i).getQuality() + 1);
+						}
 					}
+
+					if (items.get(i).getSellIn() < 6) {
+						if (items.get(i).getQuality() < 50) {
+							items.get(i).setQuality(
+									items.get(i).getQuality() + 1);
+						}
+					}
+				}
+			}
+		}
+
+		if (!items.get(i).getName().equals("Sulfuras, Hand of Ragnaros")) {
+			items.get(i).setSellIn(items.get(i).getSellIn() - 1);
+		}
+
+		if (items.get(i).getSellIn() < 0) {
+			if (!items.get(i).getName().equals("Aged Brie")) {
+				if (!items
+						.get(i)
+						.getName()
+						.equals("Backstage passes to a TAFKAL80ETC concert")) {
+					if (items.get(i).getQuality() > 0) {
+						if (!items.get(i).getName()
+								.equals("Sulfuras, Hand of Ragnaros")) {
+							items.get(i).setQuality(
+									items.get(i).getQuality() - 1);
+						}
+					}
+				} else {
+					items.get(i).setQuality(
+							items.get(i).getQuality()
+									- items.get(i).getQuality());
 				}
 			} else {
 				if (items.get(i).getQuality() < 50) {
 					items.get(i).setQuality(items.get(i).getQuality() + 1);
-
-					if (items
-							.get(i)
-							.getName()
-							.equals("Backstage passes to a TAFKAL80ETC concert")) {
-						if (items.get(i).getSellIn() < 11) {
-							if (items.get(i).getQuality() < 50) {
-								items.get(i).setQuality(
-										items.get(i).getQuality() + 1);
-							}
-						}
-
-						if (items.get(i).getSellIn() < 6) {
-							if (items.get(i).getQuality() < 50) {
-								items.get(i).setQuality(
-										items.get(i).getQuality() + 1);
-							}
-						}
-					}
-				}
-			}
-
-			if (!items.get(i).getName().equals("Sulfuras, Hand of Ragnaros")) {
-				items.get(i).setSellIn(items.get(i).getSellIn() - 1);
-			}
-
-			if (items.get(i).getSellIn() < 0) {
-				if (!items.get(i).getName().equals("Aged Brie")) {
-					if (!items
-							.get(i)
-							.getName()
-							.equals("Backstage passes to a TAFKAL80ETC concert")) {
-						if (items.get(i).getQuality() > 0) {
-							if (!items.get(i).getName()
-									.equals("Sulfuras, Hand of Ragnaros")) {
-								items.get(i).setQuality(
-										items.get(i).getQuality() - 1);
-							}
-						}
-					} else {
-						items.get(i).setQuality(
-								items.get(i).getQuality()
-										- items.get(i).getQuality());
-					}
-				} else {
-					if (items.get(i).getQuality() < 50) {
-						items.get(i).setQuality(items.get(i).getQuality() + 1);
-					}
 				}
 			}
 		}
