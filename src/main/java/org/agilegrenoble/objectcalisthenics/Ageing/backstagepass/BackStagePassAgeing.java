2ce43cad7bdf44043cbbc832805f76efcaf52b4d
Johan Martinsson <martinsson.johan@gmail.com>
2012-10-08 00:09:33 +0200

    got rid of the first class collection, it was too much

--- src/main/java/org/agilegrenoble/objectcalisthenics/Ageing/backstagepass/BackStagePassAgeing.java
+++ src/main/java/org/agilegrenoble/objectcalisthenics/Ageing/backstagepass/BackStagePassAgeing.java
@@ -1,18 +1,35 @@
 package org.agilegrenoble.objectcalisthenics.Ageing.backstagepass;
 
+import java.util.NavigableMap;
+import java.util.TreeMap;
+
 import org.agilegrenoble.objectcalisthenics.Ageing.Ageing;
+import org.agilegrenoble.objectcalisthenics.Ageing.backstagepass.IncreaseStrategy.IncreaseByOne;
+import org.agilegrenoble.objectcalisthenics.Ageing.backstagepass.IncreaseStrategy.IncreaseByThree;
+import org.agilegrenoble.objectcalisthenics.Ageing.backstagepass.IncreaseStrategy.IncreaseByTwo;
+import org.agilegrenoble.objectcalisthenics.Ageing.backstagepass.IncreaseStrategy.ResetToZero;
 import org.agilegrenoble.objectcalisthenics.quality.Quality;
 
 public class BackStagePassAgeing extends Ageing {
 
-    private BackStagePassAgeingStrategies strategies = new BackStagePassAgeingStrategies();
+    private NavigableMap<Integer,IncreaseStrategy> strategies;
 
     public BackStagePassAgeing(int daysBeforeConcert) {
         super(daysBeforeConcert);
+        strategies = new TreeMap<Integer, IncreaseStrategy>() {{
+            put(Integer.MIN_VALUE, new ResetToZero());
+            put(0, new IncreaseByThree());
+            put(5, new IncreaseByTwo());
+            put(10, new IncreaseByOne());
+        }};
     }
 
     public void advanceOneDay(Quality quality) {
-        strategies.getFor(daysBefore).applyTo(quality);
+        strategy().applyTo(quality);
         updateDaysBefore();
+    }
+
+    private IncreaseStrategy strategy() {
+        return strategies.lowerEntry(daysBefore).getValue();
     }
 }
